ef5e68c5a0313e0769388468e3b5a555daf0f629
Jayshree Anandakumar <jayshree@streamoid.com>
2017-05-13 16:33:39 +0530

    Trello utils

---
+++ lib/trello_utils.rb
@@ -0,0 +1,18 @@
+require 'trello'
+
+class TrelloUtils
+
+  def initialize
+    Trello.configure do |config|
+      config.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
+      config.member_token = ENV['TRELLO_MEMBER_TOKEN']
+    end
+  end
+
+  def fetch_cards(account)
+  end
+
+  def fetch_user
+    bob = Trello::Member.find("bobtester")
+  end
+end
